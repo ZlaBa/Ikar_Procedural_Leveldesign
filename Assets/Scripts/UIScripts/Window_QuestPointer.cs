aaa2e462b12f5b0b6c7a46ffe14f47ffe87a5877
Mario <mario.bernasconi@stud.hslu.ch>
2020-09-23 18:33:53 +0200

    Versuche eine Art Wegpunkt zum Shuttle zu erstellen.

---
+++ Assets/Scripts/UIScripts/Window_QuestPointer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window_QuestPointer : MonoBehaviour
+{
+    public Rigidbody2D WaypointCircle;
+    public Camera cam;
+}
