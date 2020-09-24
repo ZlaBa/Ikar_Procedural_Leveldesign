5d173cc54604c7a1e735cb88c8c6b6b52bb7494c
Mario <mario.bernasconi@stud.hslu.ch>
2020-09-24 17:40:41 +0200

    Kompass hinzugefügt.

--- Assets/Scripts/UIScripts/Window_QuestPointer.cs
+++ Assets/Scripts/UIScripts/Window_QuestPointer.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Window_QuestPointer : MonoBehaviour
 {
+    public Rigidbody2D Parent;
     public Rigidbody2D WaypointCircle;
     public Camera cam;
+
+    public Vector2 shuttle;
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        WaypointCircle.position = Parent.position;
+        Vector2 circleDir = shuttle - WaypointCircle.position;
+        float angle = Mathf.Atan2(circleDir.y, circleDir.x) * Mathf.Rad2Deg - 90f;
+        WaypointCircle.rotation = angle;
+    }
 }
