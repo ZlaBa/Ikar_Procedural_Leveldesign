0910724edd7c13fa2fc8bd9122cbeda94937bf88
Mario <mario.bernasconi@stud.hslu.ch>
2020-10-21 00:20:41 +0200

    Versuch mit erweitertem Tilemap die Karte zu optimieren.

---
+++ Assets/Scripts/PlayerScripts/SpawnerScript2.cs
@@ -0,0 +1,60 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SpawnerScript2 : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+
+    public Grid grid;
+    public Tilemap IsoGroundMap;
+    //public Transform spawnPos;
+    public GameObject Player;
+    private GameData _gameData;
+    /*
+    public void setBool()
+        {
+        playerSpawned = true;
+        }
+        */
+    void Update()
+    {
+        bool gameActive = true;
+        bool playerSpawned = false;
+        if (gameActive == true)
+        {
+            if (playerSpawned == false)/*(Input.GetKeyDown(KeyCode.Keypad1))*/
+            {
+                Debug.Log("Gstartet");
+                _gameData = GameObject.FindWithTag("GameData").GetComponent<GameData>();
+                Debug.Log(_gameData.GetWidth());
+
+                IsoGroundMap.CompressBounds();
+
+                for (int x = 0; x < _gameData.GetWidth(); x++)
+                {
+
+                    for (int y = 0; y < _gameData.GetHeight(); y++)
+                    {
+                        var feld = _gameData.GetFeld(x, y);
+                        Debug.Log(_gameData.GetFeld(x, y));
+                        if (feld.Terrain == FeldTerrain.Wiese)
+                        {
+                            Player.layer = 6;
+                            Instantiate(Player, IsoGroundMap.GetCellCenterWorld(new Vector3Int(x, y, 0)), Quaternion.identity);
+                            Debug.Log("Player was spawned at X: " + (IsoGroundMap.GetCellCenterWorld(new Vector3Int(x, y, 0)).x) + " Y: " + (IsoGroundMap.GetCellCenterWorld(new Vector3Int(x, y, 0)).y) + "Q: " + (Quaternion.identity) + "!");
+                            //playerActive = true;
+                            return;
+                           
+                        }
+                    }
+                }
+            }
+        }
+        playerSpawned = true;
+    }
+}
+
+
