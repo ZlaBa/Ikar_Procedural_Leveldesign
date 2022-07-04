ccbfa823a0350767ccface0110c08aa7b9c9c4ea
Mario <mario.bernasconi@stud.hslu.ch>
2022-07-05 00:21:05 +0200

    Spieleranimationen im Projekt integriert. Versuch die Animationen mit einem neuen Controllerscript einzufügen.

---
+++ Assets/Scripts/PlayerScripts/Player_Controller.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    //Inserts
+    public Rigidbody2D body;
+    public SpriteRenderer SpriteRenderer;
+
+    //Spriteliste
+    //WALK
+    public List<Sprite> Walk_S_Sprites;
+    public List<Sprite> Walk_SW_Sprites;
+    public List<Sprite> Walk_W_Sprites;
+    public List<Sprite> Walk_NW_Sprites;
+    public List<Sprite> Walk_N_Sprites;
+    public List<Sprite> Walk_NE_Sprites;
+    public List<Sprite> Walk_E_Sprites;
+    public List<Sprite> Walk_SE_Sprites;
+
+    //IDLE
+    public List<Sprite> Idle_S_Sprites;
+    public List<Sprite> Idle_SW_Sprites;
+    public List<Sprite> Idle_W_Sprites;
+    public List<Sprite> Idle_NW_Sprites;
+    public List<Sprite> Idle_N_Sprites;
+    public List<Sprite> Idle_NE_Sprites;
+    public List<Sprite> Idle_E_Sprites;
+    public List<Sprite> Idle_SE_Sprites;
+
+    //Variablen
+    public float walkSpeed;
+    public int frameRate;
+
+    float idleTime;
+    //Spriteliste ENDE
+
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get direction of input
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        //set walkbased direction
+        body.velocity = direction * walkSpeed;
+
+        List<Sprite> directionSprites = GetSpriteDirection();
+
+        if (directionSprites != null) //holding a direction
+        {
+            float playTime = Time.time - idleTime; //time walking
+            int totalFrames = (int)(playTime * frameRate); //total frames since start
+            int frame = totalFrames % directionSprites.Count; //current frame
+
+            SpriteRenderer.sprite = directionSprites[frame];
+        }
+        else // holding nothing, input is neutral
+        {
+            idleTime = Time.time;
+        }
+    }
+
+    List<Sprite> GetSpriteDirection()
+    {
+        List<Sprite> selectedSprites = null;
+
+        if (direction.y > 0) //north
+        {
+            if (direction.x > 0) //east
+            {
+                selectedSprites = Walk_NE_Sprites;
+            }
+            else
+            {
+                selectedSprites = Walk_N_Sprites;
+            }
+        }
+        else if (direction.y < 0) //south 
+        {
+            if (direction.x > 0) //east
+            {
+                selectedSprites = Walk_SE_Sprites;
+            }
+            else
+            {
+                selectedSprites = Walk_S_Sprites;
+            }
+        }
+        return selectedSprites;
+    }
+}
+
--- Assets/Scripts/WordleditorScripts/TileAutomata.cs
+++ Assets/Scripts/WordleditorScripts/TileAutomata.cs
@@ -7,11 +7,13 @@ using Assets;
 
 public class TileAutomata : MonoBehaviour
 {
+    /*
     //Player Spawn Data START
     public GameObject SpawningObject;
     public Tilemap ObjTilemap;
     public int maxPlayerAmount = 1;
     //Player Spawn Data END
+    */
 
     [Range(0, 100)]
     public int iniWald;
@@ -357,7 +359,7 @@ public class TileAutomata : MonoBehaviour
                 {
                     var feld = new Feld { Terrain = FeldTerrain.Wiese };
                     _gameData.SetFeld(feld, x, y);
-                    activateSpawnPlayer();
+                    //activateSpawnPlayer();
                 }
             }
         }
@@ -366,7 +368,7 @@ public class TileAutomata : MonoBehaviour
     {
         doSim(rechenRunden);
     }
-
+    /*
     public void activateSpawnPlayer()
     {
         //Player Spawn void Start() START
@@ -374,6 +376,7 @@ public class TileAutomata : MonoBehaviour
         for (playerAmount = 0; playerAmount < maxPlayerAmount; playerAmount++)
         {
             SpawnPlayer();
+            playerAmount = 1; //NEUE ZEILE 04-07-22
         }
         //Player Spawn void Start() END
     }
@@ -381,8 +384,8 @@ public class TileAutomata : MonoBehaviour
     //Player Spawn Function START
     public void SpawnPlayer()
     {
-        int randomX = Random.Range(-64 / 4, 64 / 4);
-        int randomY = Random.Range(-64 / 4, 64 / 4);
+        int randomX = Random.Range(-16 / 4, 16 / 4); //NEUE ZEILE 04-07-22 | MAX RANGE / 4
+        int randomY = Random.Range(-16 / 4, 16 / 4); //NEUE ZEILE 04-07-22 | MAX RANGE / 4
 
         Vector3Int cellPosition = new Vector3Int(randomX, randomY, 0);
 
@@ -391,6 +394,7 @@ public class TileAutomata : MonoBehaviour
         Instantiate(SpawningObject, position, Quaternion.identity);
         //Debug.Log("Piece spawned at" + " " + position);
     }
+    */
     //Player Spawn Function END
 
     // Update is called once per frame
